b879b5a6083f75d135bf05e0aaea5fb0e8b8b85e
nmunatsibw <mcturnergarai@gmail.com>
2024-02-01 11:11:40 +0200

    Revert ":sparkles: feat(CRFs): Refactor and clean up code for improved readability"
    This reverts commit fc42198ccf9ee21ccd5e125759cbec8e764a0996.

--- flourish_metadata_rules/tests/models.py
+++ flourish_metadata_rules/tests/models.py
@@ -6,22 +6,26 @@ from edc_base.utils import get_utcnow
 
 
 class MaternalDataset(BaseUuidModel):
+
     subject_identifier = models.CharField(max_length=25)
 
     delivdt = models.DateField(null=True, blank=True)
 
 
 class ChildDataset(BaseUuidModel):
+
     subject_identifier = models.CharField(max_length=25)
 
 
 class ScreeningPriorBhpParticipants(BaseUuidModel):
+
     screening_identifier = models.CharField(max_length=25)
 
     flourish_participation = models.CharField(max_length=25)
 
 
 class SubjectConsent(BaseUuidModel):
+
     consent_datetime = models.DateTimeField(
         default=get_utcnow)
 
@@ -33,6 +37,7 @@ class SubjectConsent(BaseUuidModel):
 
 
 class ChildAssent(BaseUuidModel):
+
     consent_datetime = models.DateTimeField(
         default=get_utcnow)
 
@@ -44,6 +49,7 @@ class ChildAssent(BaseUuidModel):
 
 
 class ChildDummySubjectConsent(BaseUuidModel):
+
     consent_datetime = models.DateTimeField(
         default=get_utcnow)
 
@@ -61,14 +67,17 @@ class ChildDummySubjectConsent(BaseUuidModel):
 
 
 class AntenatalEnrollment(BaseUuidModel):
+
     subject_identifier = models.CharField(max_length=25)
 
 
 class Appointment(BaseUuidModel):
+
     subject_identifier = models.CharField(max_length=25)
 
 
 class MaternalDelivery(BaseUuidModel):
+
     subject_identifier = models.CharField(max_length=25)
 
     delivery_datetime = models.DateTimeField(null=True, blank=True)
@@ -77,6 +86,7 @@ class MaternalDelivery(BaseUuidModel):
 
 
 class MaternalVisit(BaseUuidModel):
+
     appointment = models.ForeignKey(Appointment,
                                     on_delete=PROTECT)
 
@@ -84,16 +94,15 @@ class MaternalVisit(BaseUuidModel):
 
 
 class ChildVisit(BaseUuidModel):
+
     appointment = models.ForeignKey(Appointment,
                                     on_delete=PROTECT)
-
-    report_datetime = models.DateTimeField(
-        default=get_utcnow())
 
     subject_identifier = models.CharField(max_length=25)
 
 
 class CaregiverChildConsent(BaseUuidModel):
+
     consent_datetime = models.DateTimeField(
         default=get_utcnow)
 
@@ -103,6 +112,7 @@ class CaregiverChildConsent(BaseUuidModel):
 
 
 class CyhuuPreEnrollment(BaseUuidModel):
+
     maternal_visit = models.ForeignKey(MaternalVisit,
                                        on_delete=PROTECT)
 
@@ -110,15 +120,15 @@ class CyhuuPreEnrollment(BaseUuidModel):
 
 
 class HivRapidTestCounseling(BaseUuidModel):
+
     maternal_visit = models.ForeignKey(MaternalVisit,
                                        on_delete=PROTECT)
 
     subject_identifier = models.CharField(max_length=25)
 
     result = models.CharField(max_length=3)
-
-
 class RelationshipFatherInvolvement(BaseUuidModel):
+
     maternal_visit = models.ForeignKey(MaternalVisit,
                                        on_delete=PROTECT)
 
