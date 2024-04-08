0a01dce3d1b8359d6641df4ce05995f43100dcc0
Keba462 <79043038+Keba462@users.noreply.github.com>
2024-04-08 15:31:27 +0200

    adding a metadata rule

--- flourish_metadata_rules/predicates/caregiver_predicates.py
+++ flourish_metadata_rules/predicates/caregiver_predicates.py
@@ -6,7 +6,7 @@ from edc_base.utils import age, get_utcnow
 from edc_constants.constants import IND, NEG, PENDING, POS, UNK, YES
 from edc_metadata_rules import PredicateCollection
 from edc_reference.models import Reference
-
+from flourish_caregiver.constants import BREASTFEED_ONLY
 from flourish_caregiver.helper_classes import MaternalStatusHelper
 from flourish_caregiver.helper_classes.utils import get_child_subject_identifier_by_visit
 
@@ -377,8 +377,8 @@ class CaregiverPredicates(PredicateCollection):
                             child_age = age(
                                 child_consent.child_dob, get_utcnow())
                             child_age_in_months = ((
-                                                           child_age.years * 12) +
-                                                   child_age.months)
+                                child_age.years * 12) +
+                                child_age.months)
                             if child_age_in_months < 2:
                                 try:
                                     last_tb_bj = tb_screening_form_objs.latest(
@@ -407,13 +407,13 @@ class CaregiverPredicates(PredicateCollection):
             return False
         else:
             take_off_schedule = (
-                    visit_screening.have_cough == YES or
-                    visit_screening.cough_duration == '=>2 week' or
-                    visit_screening.fever == YES or
-                    visit_screening.night_sweats == YES or
-                    visit_screening.weight_loss == YES or
-                    visit_screening.cough_blood == YES or
-                    visit_screening.enlarged_lymph_nodes == YES
+                visit_screening.have_cough == YES or
+                visit_screening.cough_duration == '=>2 week' or
+                visit_screening.fever == YES or
+                visit_screening.night_sweats == YES or
+                visit_screening.weight_loss == YES or
+                visit_screening.cough_blood == YES or
+                visit_screening.enlarged_lymph_nodes == YES
             )
             return take_off_schedule
 
@@ -527,10 +527,27 @@ class CaregiverPredicates(PredicateCollection):
         ).order_by('-report_datetime').first()
         if latest_obj:
             return (
-                    latest_obj.cough_duration == '≥ 2 weeks' or
-                    latest_obj.fever == YES or
-                    latest_obj.sweats == YES or
-                    latest_obj.weight_loss == YES
+                latest_obj.cough_duration == '≥ 2 weeks' or
+                latest_obj.fever == YES or
+                latest_obj.sweats == YES or
+                latest_obj.weight_loss == YES
 
             )
         return False
+
+    def func_show_breast_milk_crf(self, visit=None, **kwargs):
+        """Returns true if participant is breastfeeding of breastfeeding and formula feeding.
+        """
+        child_subject_identifier = get_child_subject_identifier_by_visit(visit)
+
+        if self.enrolled_pregnant(visit=visit, **kwargs):
+            birth_form_model_cls = django_apps.get_model(
+                f'{self.app_label}.maternaldelivery')
+            try:
+                birth_form_obj = birth_form_model_cls.objects.get(
+                    subject_identifier=visit.subject_identifier,
+                    child_subject_identifier=child_subject_identifier)
+                return birth_form_obj.feeding_mode == BREASTFEED_ONLY or birth_form_obj.feeding_mode == 'mixed_feeding'
+
+            except birth_form_model_cls.DoesNotExist:
+                return False
