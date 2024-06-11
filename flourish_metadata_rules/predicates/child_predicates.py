e08cb372946244217639652e4a5f449a6715f705
nmunatsibw <mcturnergarai@gmail.com>
2024-06-11 19:07:35 +0200

    (Feature): Add TB screening and HEU status functions
    Two new functions have been added to the child predicates. The `func_child_tb_screening_required` function checks if a child's TB screening is needed based on a series of tests. In addition to the TB screening function, the `func_heu_status_disclosed` has been introduced to determine the HIV exposure of a child whether the HEU status is disclosed or not.
    
    Signed-off-by: nmunatsibw <mcturnergarai@gmail.com>
    
    (cherry picked from commit 3a1834955fdf9805e253bae76a26fd76b6c8ad06)
    Signed-off-by: nmunatsibw <mcturnergarai@gmail.com>

--- flourish_metadata_rules/predicates/child_predicates.py
+++ flourish_metadata_rules/predicates/child_predicates.py
@@ -525,6 +525,7 @@ class ChildPredicates(PredicateCollection):
         child_age_in_months = (child_age.years * 12) + child_age.months
 
         hiv_status = self.get_latest_maternal_hiv_status(visit=visit).hiv_status
+
         if (hiv_status == POS and self.func_consent_study_pregnant(visit=visit)):
             if (self.newly_enrolled(visit=visit)
                     and visit.visit_code in ['2001', '2003', '3000', '3000A', '3000B',
