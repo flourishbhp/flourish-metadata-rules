baa0fda619dd3def29778daa8439dee87a4a9701
Ame Diphoko <diphokoame@gmail.com>
2024-07-30 08:31:48 +0200

    update: correct child tb screening imports

--- flourish_metadata_rules/metadata_rules/__init__.py
+++ flourish_metadata_rules/metadata_rules/__init__.py
@@ -27,5 +27,5 @@ from .child_rule_groups import TbAdolEngagementRuleGroup
 from .child_rule_groups import TbAdolInterviewRuleGroup
 from .child_rule_groups import TbLabResultsAdolRuleGroup
 from .child_rule_groups import TbPresenceHouseholdMembersAdolRuleGroup
-from .child_rule_groups import TbScreeningRules
+from .child_rule_groups import ChildTbScreeningRules
 from .child_rule_groups import TbVisitScreeningAdolRuleGroup
--- flourish_metadata_rules/metadata_rules/child_rule_groups/__init__.py
+++ flourish_metadata_rules/metadata_rules/child_rule_groups/__init__.py
@@ -14,3 +14,4 @@ from .tb_adolescent_rules import HivTestingAdolRuleGroup
 from .tb_adolescent_rules import TbLabResultsAdolRuleGroup
 from .tb_adolescent_rules import TbPresenceHouseholdMembersAdolRuleGroup
 from .tb_adolescent_rules import TbVisitScreeningAdolRuleGroup
+from .tb_screening_rules import TbScreeningRules as ChildTbScreeningRules
